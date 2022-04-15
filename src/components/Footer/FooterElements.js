a08f1b7a44d070cff54457440740cedd681f714e
hamiltcs2 <hamiltcs2@gmail.com>
2022-04-15 17:06:52 -0400

    changed rights

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const FooterContainer = styled.footer`
   background-color: #101522;
+  margin-bottom: 0;
 `;
 
 export const FooterWrap = styled.div`
