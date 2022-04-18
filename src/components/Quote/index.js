7da84c2eba1545b805b14f33c2229d2da456d955
hamiltcs2 <hamiltcs2@gmail.com>
2022-04-18 19:36:41 -0400

    finishing request form

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Form.css';
 import FormSignup from './FormSignup';
 import FormSuccess from './FormSuccess';
-import axios from 'axios';
 import {
   Container,
   FormContainer,
@@ -16,30 +15,8 @@ const Quote = ({services}) => {
   }
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const submitForm = (values) => {
+  const submitForm = () => {
     setIsSubmitted(true);
-      const payload = {
-        name: values.name,
-        email: values.email,
-        phone: values.phone,
-        address: values.address,
-        city: values.city,
-        zipcode: values.zipcode,
-        service: values.service,
-        info: values.info
-    };
-    axios({
-      url: 'api/save',
-      method: 'POST',
-      data: payload
-    })
-    .then(() => {
-      console.log('Data has been sent to the server');
-    })
-    .catch((error) => {
-      console.log('Internal server error');
-      console.log('Error is '+error);
-    });
   }
   return (
     <>
--- src/components/Quote/useForm.js
+++ src/components/Quote/useForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+import axios from 'axios';
 const useForm = (callback, validate, services, options) => {
   const [values, setValues] = useState({
     name: '',
@@ -42,10 +42,33 @@ const useForm = (callback, validate, services, options) => {
         })
       }
       if (Object.keys(errors).length === 0 && isSubmitting) {
-        callback(values);
+        const payload = {
+          name: values.name,
+          email: values.email,
+          phone: values.phone,
+          address: values.address,
+          city: values.city,
+          zipcode: values.zipcode,
+          service: values.service,
+          info: values.info
+      };
+      axios({
+        url: 'api/save',
+        method: 'POST',
+        data: payload
+      })
+      .then(() => {
+        console.log('Data has been sent to the server');
+      })
+      .catch((error) => {
+        console.log('Internal server error');
+        console.log('Error is '+error);
+      });
+        callback();
       }
     },
-    [callback, errors, isSubmitting, options, services, values]
+    [callback, errors, isSubmitting, options, services, values.name, values.email, values.phone,
+    values.address, values.city, values.zipcode, values.service, values.info]
   );
 
   return { handleChange, handleSubmit, values, errors };
